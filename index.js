19273f3776bd5b75b57882fbec139cafd8142026
Amir <jopp@Jopps-MacBook-Pro.local>
2019-08-09 21:16:32 +0430

    rebuild project to publish

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { persistStore } from 'redux-persist';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import storage from 'redux-persist/lib/storage';
 export default function create(generator) {
   var sagaMiddleware = createSagaMiddleware();
   var middleWares = [sagaMiddleware];
-  var reducers = generator.getReducers();
+  var reducers = combineReducers(generator.getReducers());
   var sagas = generator.getSagas();
   var whiteList = generator.getWhiteList();
   var store = createStore(reducers, applyMiddleware.apply(void 0, middleWares));
