d0b2757b74f61a8a7c07d35caa6adaac5def1c24
Amir <jopp@Jopps-MacBook-Pro.local>
2019-08-09 22:13:02 +0430

    rebuild project + change version to 1.0.12

--- build/index.js
+++
@@ -1,6 +0,0 @@
-import createStore from './createStore';
-import { connect, Provider } from 'react-redux';
-import ReduxGenerator from './generator';
-import { PersistGate } from 'redux-persist/es/integration/react';
-var generator = new ReduxGenerator();
-export { PersistGate, Provider, connect, generator, createStore };
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-import './build';
+const createStore = require('./build/createStore');
+const { connect, Provider } = require('react-redux');
+const ReduxGenerator = require('./build/generator');
+const { PersistGate } = require('redux-persist/es/integration/react');
+
+const generator = new ReduxGenerator();
+module.exports = { PersistGate, Provider, connect, generator, createStore };
